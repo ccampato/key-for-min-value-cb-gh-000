ae57ce7fe73ae0cea51391133d3c9f73d49a42ae
Caio Sanches <ccampato@gmail.com>
2018-04-25 02:46:45 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,16 +5,16 @@ def key_for_min_value(name_hash)
   c = []
   name_hash.each { |k,v| c << v }
 
-  if c[0] > c[1] || c[0] > c[2]
-    c.delete(0)
-  elsif c[1] > c[0] || c[1] > c[2]
-    c.delete(1)
-  elsif c[2] > c[0] || c[2] > c[1]
-    c.delete(2)
-  end
+  s = if c[0] > c[1] || c[0] > c[2]
+        c.delete(0)
+      elsif c[1] > c[0] || c[1] > c[2]
+        c.delete(1)
+      elsif c[2] > c[0] || c[2] > c[1]
+        c.delete(2)
+      end
 
   name_hash.find do |k,v|
-    v == c
+    v == s
     return k
   end
 end
