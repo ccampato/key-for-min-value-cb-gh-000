09b70eb7b5ac6d53395449062751c2e5aee4b51d
Caio Sanches <ccampato@gmail.com>
2018-04-25 03:05:33 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,13 +5,7 @@ def key_for_min_value(name_hash)
   c = []
   name_hash.each { |k,v| c << v }
 
-  if (c[0] < c[1] && c[0] < c[2])
-    smallest = c.delete(0)
-  elsif (c[1] < c[2] && c[1] < c[0])
-    smallest = c.delete(1)
-  elsif (c[2] < c[1] && c[2] < c[0])
-    smallest = c.delete(2)
-  end
+  smallest = c.min
 
   name_hash.select { |k,v| return k if v == smallest }
 end
