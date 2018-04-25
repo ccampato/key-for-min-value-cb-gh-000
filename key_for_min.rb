dea0e1b28dbf792f76100e41d3fbff59262ec4c1
Caio Sanches <ccampato@gmail.com>
2018-04-25 02:41:25 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -13,8 +13,8 @@ def key_for_min_value(name_hash)
     c.delete(2)
   end
 
-  lowest = name_hash.find do |k,v| 
-    name_hash[k] <= c
+  lowest = name_hash.find do |k,v|
+    v <= c
     return k
   end
   lowest
